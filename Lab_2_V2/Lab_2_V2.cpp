780198cf7b4134bf4af5d6b8991b4bfc8b5db9f7
Trust Fund <atstofko17@gmail.com>
2024-03-14 08:54:15 -0500

    V2

--- Lab_2_V2/Lab_2_V2.cpp
+++ Lab_2_V2/Lab_2_V2.cpp
@@ -15,16 +15,18 @@ int main()
 	withdraw = 0;
 	deposit = 0;
 	cout << "Welcome to Scooby\n";
-	cout << "This enter the name associated with your account with is --> ";
+	cout << "Enter the name is account is associated with --> ";
 	cin >> name;
-	cout << "Enter the ammount total inside of your account --> ";
+	cout << "Enter the total ammount inside of your account --> ";
 	cin >> balance;
 	cout << "Total Balance --> " << balance << endl;
-	cout << "Enter option:\n-Withdraw\n-Deposit\n-End Transaction\n--> ";
+	cout << "Enter option:\n-Withdraw\n-Deposit\n-Finish Transaction\n--> ";
 	BankAccount person(name, balance, withdraw, deposit);
 	cin >> input;
-	if (input == "Withdraw")
+	//Input for the withdraw
+	if (input == "Withdraw" or "withdraw")
 	{
+		withdraw:
 		cout << setfill('*') << setw(static_cast<streamsize>(SCREEN_WIDTH) + 1) << " " << setfill(' ');
 		cout << "Please enter the withdraw ammount --> ";
 		cin >> withdraw;
@@ -33,23 +35,19 @@ int main()
 		cout << "Total Balance --> " << person.getbalance() << endl;
 		cout << "Enter option:\n-Continue Withdrawing (1)\n-End Withdraw (2)\n--> ";
 		cin >> input;
-	}
-	while (input == "Continue Withdrawing" or "1")
-	{
-		cout << "Please enter the withdraw ammount --> ";
-		cin >> withdraw;
-		person.Setwithdrawammount(withdraw);
-		person.Withdraw(withdraw);
-		cout << "Total Balance --> " << person.getbalance() << endl;
-		cout << "Enter option:\n-Continue Withdrawing (1)\n-End Withdraw (2)\n--> ";
-		cin >> input;
-		if (input == "2")
+		if (input == "1")
 		{
-			return 0;
+			goto withdraw;
 		}
-	}	
-	if (input == "Deposit")
+		else if (input == "2")
+		{
+			(input = "End Withdraw");
+		}
+	}
+	//Input for the deposit
+	if (input == "Deposit" or "deposit")
 	{
+		deposit:
 		cout << setfill('*') << setw(static_cast<streamsize>(SCREEN_WIDTH) + 1) << " " << setfill(' ');
 		cout << "Please enter the deposit ammount --> ";
 		cin >> deposit;
@@ -58,40 +56,38 @@ int main()
 		cout << "Total Balance --> " << person.getbalance() << endl;
 		cout << "Enter option:\n-Continue Depositing (1)\n-End Deposit (2)\n--> ";
 		cin >> input;
-	}
-	while (input == "Continue Depositing" or "1")
-	{
-		cout << "Please enter the deposit ammount --> ";
-		cin >> deposit;
-		person.Setdepositammount(deposit);
-		person.Deposit(deposit);
-		cout << "Total Balance --> " << person.getbalance() << endl;
-		cout << "Enter option:\n-Continue Depositing (1)\n-End Deposit (2)\n--> ";
-		cin >> input;
-		bool input;
-		if (input = 2)
+		if (input == "1")
 		{
-			cout << setfill('*') << setw(static_cast<streamsize>(SCREEN_WIDTH) + 1) << " " << setfill(' ');
-			cout << "Total Balance --> " << person.getbalance() << endl;
-			cout << "Enter option:\n-Withdraw\n-Deposit\n-End Transaction\n--> ";
-			cin >> input;
-			break;
+		goto deposit;
 		}
-		if (input = 1)
+		else if (input == "2")
 		{
-			break;
+			(input = "End Deposit");
 		}
 	}
-	if (input == "End Withdraw" or "End Deposit" or "2")
+	//Input after ending each with withdraw or deposit
+	else if (input == "End Withdraw" or "End Deposit")
 	{
 		cout << setfill('*') << setw(static_cast<streamsize>(SCREEN_WIDTH) + 1) << " " << setfill(' ');
 		cout << "Total Balance --> " << person.getbalance() << endl;
-		cout << "Enter option:\n-Withdraw\n-Deposit\n-End Transaction\n--> ";
+		cout << "Enter option:\n-Withdraw\n-Deposit\n-Finish Transaction\n--> ";
 		cin >> input;
+		if (input == "Withdraw" or "1")
+		{
+			goto withdraw;
+		}
+		if (input == "Deposit" or "2")
+		{
+			goto deposit;
+		}
 	}
-	if (input == "End Transaction")
+	if (input == "Finish Transaction")
 	{
+		//Ending
 		cout << setfill('*') << setw(static_cast<streamsize>(SCREEN_WIDTH) + 1) << " " << setfill(' ');
-		cout << "\nThanks, have a great day\n";
+		cout << "\nThanks, have a great day\n" << endl;
+	}
+	else {
+		return 0;
 	}
 }
